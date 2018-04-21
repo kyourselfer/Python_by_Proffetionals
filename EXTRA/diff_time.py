d3ebdd0559f420e3ca27c2f00625a7af8309226c
kyourselfer@gmail.com <Razzvitie_1985$>
2018-04-21 22:13:12 +0300

    from BASH to PYTHON3 (WebServer1)

---
+++ EXTRA/diff_time.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    import time
+#
+s1 = time.time()
+# a = 0
+# while a != 100000000:
+#     # print(a)
+#     a += 1
+# print(time.time())
+print("--- %s seconds ---" % (time.time() - s1))
